c78ae862c1dc5e7e11e43b7a6ff283c786f0d55d
Richard Paulo Villanueva <91730394+Richard9703@users.noreply.github.com>
2022-06-26 21:20:44 +0100

    Add files via upload

---
+++ django/env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DATABASE_URL'] = "postgres://xlrejegafquwcz:5ad53a0d5080001c72f0d22f70be5d00f8ec4d5d67dcacd19b9560f925f950de@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/ddu36rctu3ac4"
+os.environ['SECRET_KEY'] = "random1234!"
+os.environ["CLOUDINARY_URL"] = "cloudinary://265181513524478:HaeGBHrdh5k40vmTrI_p8RK21Zc@db78ie5ij"
